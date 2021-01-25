16929f97d9019e1c5cd2f744179faea59892096c
afosh <afoshidris@gmail.com>
2021-01-25 22:38:16 +0200

    project routes and controllers updates

---
+++ server/controllers/projectControllers/projectController.js
@@ -0,0 +1,9 @@
+exports.uploadProject = (req, res) => {};
+
+function projectStructure(projectId, userId) {}
+
+/*
+user -> last added project -> get that project id + add it to the package id
+user -> project ->
+
+*/
--- server/routes/projectsRoutes.js
+++ server/routes/projectsRoutes.js
@@ -2,8 +2,14 @@ var express = require("express");
 var Router = express.Router();
 
 const projectsTimeLineController = require("../controllers/timelineControllers/projectsTimeLineController");
-
+const hanlderController = require("../controllers/projectControllers/projectHandlerController");
+const projectController = require("../controllers/projectControllers/projectController");
 //Get Projects TimeLine
 Router.get("/v1/", projectsTimeLineController.projectsTimeLine_get);
 
+Router.post(
+  "/v2/upload",
+
+  projectController.uploadProject
+);
 module.exports = Router;
